a8a98d967d4133577671e41b2c520289f94ea097
Amine ELMAROUFI <elmaroufi.amine@gmail.com>
2023-04-19 15:02:03 +0200

    update test command

--- jest.config.js
+++ jest.config.js
@@ -19,4 +19,8 @@ module.exports = {
   // This is the only part which you can keep
   // from the above linked tutorial's config:
   cacheDirectory: '.jest/cache',
+  reporters: [
+    'default',
+    ['jest-junit', {outputDirectory: 'test-results', outputName: 'junit.xml'}],
+  ],
 };
